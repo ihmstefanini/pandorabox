a03dc7645932ccf64a06924de64895c04f19afde
Allana Alves Silva <allanaasilva95@gmail.com>
2021-09-05 03:35:23 -0300

    alteração setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
+
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
