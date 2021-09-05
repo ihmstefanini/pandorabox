c4996321162abebe670d63cc7a0a9c8a8acfc84a
Allana Alves Silva <allanaasilva95@gmail.com>
2021-09-05 01:52:05 -0300

    alteração setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"allanaasilva95@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
